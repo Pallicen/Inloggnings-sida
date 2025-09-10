0d6e466154f2514e24c7a1315a6f83cb3f9bb5d2
Alice Lindkvist <alicelindkvist@MacBook-Air-som-tillhor-Alice.local>
2025-09-10 18:14:20 +0200

    snyggat till koden sååå bra

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
-document.getElementById("välkommensida").style.display = "none";
 
+const ärInloggad = localStorage.getItem("ärInloggad");
+const välkommensida = document.getElementById("välkommensida");
+const inloggningssida = document.getElementById("inloggningssida");
 const loginButton = document.getElementById("loginButton");
 const logoutButton = document.getElementById("logoutButton");
+const fel = document.getElementById("fel");
+
+const rättAnvändarnamn = "test";
+const rättLösenord = "1234";
+
+välkommensida.style.display = "none";
+
+window.onload = function() {
+
+if (ärInloggad === "true") {
+inloggningssida.style.display = "none";
+välkommensida.style.display = "block";
+} else {
+inloggningssida.style.display = "block";
+välkommensida.style.display = "none";
+}
+};
+
 
 loginButton.addEventListener("click", function (){
-  const användarnamn = document.getElementById("användarnamn").value;
-  const lösenord = document.getElementById("lösenord").value;
-  const fel = document.getElementById("fel");
 
-  const rättAnvändarnamn = "test";
-  const rättLösenord = "1234";
+const användarnamn = document.getElementById("användarnamn").value;
+const lösenord = document.getElementById("lösenord").value;
 
   if (användarnamn === rättAnvändarnamn && lösenord === rättLösenord) {
+    localStorage.setItem("ärInloggad", "true");
 
-    document.getElementById("inloggningssida").style.display = "none";
-    document.getElementById("välkommensida").style.display = "block";
+   inloggningssida.style.display = "none";
+   välkommensida.style.display = "block";
 
   } else {
-    fel.textContent = "Fel användarnamn eller lösenord";
+    fel.textContent = "Fel användarnamn \neller lösenord";
   }
 });
 
 logoutButton.addEventListener("click", function() {
+  localStorage.removeItem("ärInloggad");
 
-    document.getElementById("inloggningssida").style.display = "block";
-    document.getElementById("välkommensida").style.display = "none";
-
-    document.getElementById("användarnamn").value = "";
-    document.getElementById("lösenord").value = "";
-    document.getElementById("fel").textContent = "";
+  const användarnamn = document.getElementById("användarnamn");
+  const lösenord = document.getElementById("lösenord");
+  
+  välkommensida.style.display = "none";
+  inloggningssida.style.display = "block";
+  
+  användarnamn.value = "";
+  lösenord.value = "";  
+  fel.textContent = "";
 
 });
+
+
